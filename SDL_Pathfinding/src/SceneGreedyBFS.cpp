dd1bdc2c7b254a56699ff5bbb6c8049a2003c8e6
binrong <binrongzheng@gmail.com>
2017-11-24 18:41:41 +0100

    commit

--- SDL_Pathfinding/src/SceneGreedyBFS.cpp
+++ SDL_Pathfinding/src/SceneGreedyBFS.cpp
@@ -99,7 +99,7 @@ void SceneGreedyBFS::update(float dtime, SDL_Event *event)
 						while ((!isValidCell(coinPosition)) || (Vector2D::Distance(coinPosition, pix2cell(agents[0]->getPosition()))<3))
 						coinPosition = Vector2D((float)(rand() % num_cell_x), (float)(rand() % num_cell_y));
 						//Creem cami un altre cop
-						//path.points = agents[0]->Behavior()->SceneGreedyBFS(graph, cell2pix(pix2cell(agents[0]->getPosition())), cell2pix(coinPosition),bridge);
+						path.points = agents[0]->Behavior()->SceneGreedyBFS(graph, cell2pix(pix2cell(agents[0]->getPosition())), cell2pix(coinPosition),bridge);
 					}
 				}
 				else
